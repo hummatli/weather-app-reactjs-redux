de15c941dea43b503336791f71878fdd2247674c
Sattar Hummatli <settarxan@gmail.com>
2017-12-05 09:55:43 +0400

    create of new conatiner called WeatherList

---
+++ src/containers/weather_list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    );
+  }
+}
+
+//Asaqidaki funksiya ile eynidir
+// function mapStateToProps(state) {
+//   return { weather: state.weather };
+// }
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
