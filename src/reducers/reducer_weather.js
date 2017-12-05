0ea40e00e4f887429ba9693b7cbf48430625c9bf
Sattar Hummatli <settarxan@gmail.com>
2017-12-05 09:43:39 +0400

    modify reducer to get fetch_weather action

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
 
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      //return state.concat([action.payload.data]);  //ES5 ile    
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
