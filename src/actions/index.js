7d7521aa49ac7ea1b295a4a51eab52f03baba0e2
Sattar Hummatli <settarxan@gmail.com>
2017-12-06 12:56:06 +0400

    add dropbox for country selection

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,8 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+export function fetchWeather(city, code) {
+  const url = `${ROOT_URL}&q=${city},${code}`;
   const request = axios.get(url);
 
   return {
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
+import countryList from '../data/country_list';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = {
+      term: '',
+      country: {
+        code: 'az',
+        name: 'Azerbaijan'
+      }
+    };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onDropdownItemChange = this.onDropdownItemChange.bind(this);
   }
 
   onInputChange(event) {
@@ -20,11 +28,34 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-    this.props.fetchWeather(this.state.term);
+    this.props.fetchWeather(this.state.term, this.state.country.code);
     this.setState({ term: '' });
   }
 
+  onDropdownItemChange(country){
+    this.setState({
+      country: {
+        code: country.iso2.toLowerCase(),
+        name: country.name
+      }
+    })
+    console.log(this.state.country);
+  }
+
   render() {
+    const countryArr = Object.entries(countryList);
+    const list = countryArr.map(c => {
+      return (
+        <a
+          onClick={() => {this.onDropdownItemChange(c[1])}}
+          className="dropdown-item"
+          href="#"
+          key={c[1].iso2}>
+          {c[1].name}
+        </a>
+      );
+    });
+
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
@@ -32,6 +63,14 @@ class SearchBar extends Component {
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange} />
+        <div className="dropdown input-group-btn">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {this.state.country.name}
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            {list}
+          </div>
+        </div>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
